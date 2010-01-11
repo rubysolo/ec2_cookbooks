4879dd6f5a35269a9dbf95213ee263a1ac5cfd12
Solomon White <rubysolo@gmail.com>
2010-01-11 09:45:08 -0700

    copy github ssh keys to rails user

--- bin/chef_runner.rb
+++ bin/chef_runner.rb
@@ -43,6 +43,11 @@ end.flatten
 active_ips << metadata['local-ipv4']
 active_ips.uniq!
 
+# SSH keys to copy to rails user
+ssh_keys = [:id_rsa, :github_rsa, :known_hosts, :authorized_keys].map do |keyname|
+  %Q{"#{keyname}": "#{IO.read("/root/.ssh/#{keyname}").gsub(/\n/, '\\n')}"}
+end.join(",\n")
+
 # generate the result app.json
 result = {
   'MEMCACHED_HOSTS'  => active_ips.map{|i| %Q{"#{i}"} }.join(', '),
@@ -51,7 +56,8 @@ result = {
   'DB_USERNAME'      => rds['username'],
   'DB_PASSWORD'      => rds['password'],
   'FACEBOOK_CONFIG'  => facebook_config.gsub(/\n/, '\\n'),
-  'S3_ASSETS_CONFIG' => s3_assets_config.gsub(/\n/, '\\n')
+  'S3_ASSETS_CONFIG' => s3_assets_config.gsub(/\n/, '\\n'),
+  'SSH_KEYS'         => ssh_keys
 }.inject(template) do |t, (search, replace)|
   t.gsub(search, replace)
 end
